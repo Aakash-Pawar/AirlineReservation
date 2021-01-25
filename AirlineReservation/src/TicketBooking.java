08fab67139f185fe7dbcdbd07d8a93b2c666a63b
Shilpa shrirang gulagule <70972022+Shilpagulagule@users.noreply.github.com>
2021-01-25 12:08:53 +0530

    4rth commit ticket booking jframe

--- AirlineReservation/src/TicketBooking.java
+++ AirlineReservation/src/TicketBooking.java
@@ -4,10 +4,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class TicketBooking extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -30,11 +34,28 @@ public class TicketBooking extends JFrame {
 	 */
 	public TicketBooking() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 888, 512);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(83, 69, 49, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(167, 66, 96, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(83, 143, 49, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(182, 140, 96, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
 	}
-
 }
